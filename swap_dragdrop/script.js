0454b2cb056b0fd1d967960425106194dd406d86
alfio pietrantuono <alfio@localhost.localdomain>
2024-11-08 12:03:57 +0100

    drag & drop con mark position

--- swap_dragdrop/script.js
+++ swap_dragdrop/script.js
@@ -2,6 +2,9 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
     var dragSrcEl = null;
     var elementAt = null;
+    const span = document.createElement('span');
+    var test;
+    span.className = 'mark';
 
     function handleDragStart(e) {
         this.style.opacity = '0.2';
@@ -16,25 +19,57 @@ document.addEventListener('DOMContentLoaded', (event) => {
         if (e.preventDefault) {
             e.preventDefault();
         }
+        // console.log(this);
         elementAt = document.elementFromPoint(e.clientX, e.clientY);
-        if (elementAt === dragSrcEl) return;
-        // console.clear();
-        // console.log('dragSrc : ', dragSrcEl.offsetLeft);
-        // console.log('elementAt before : ', elementAt.offsetLeft);
-        // console.log('diff : ', dragSrcEl.offsetLeft - elementAt.offsetLeft);
-        // console.log('width / 2: ', dragSrcEl.offsetWidth / 2);
-        if (dragSrcEl.offsetLeft - elementAt.offsetLeft <= (dragSrcEl.offsetWidth / 2)) {
-            // elementAt.style.left = '-100%';
-            // const val = dragSrcEl.offsetLeft - elementAt.offsetLeft;
-            // elementAt.style.left = `-${dragSrcEl.offsetWidth}px`;
-            // dragSrcEl.style.left = '514px';
-            elementAt.after(dragSrcEl);
+        test = document.elementFromPoint(e.clientX, e.clientY);
+        // console.log(elementAt);
+
+        // TEST: logica funzionante con un marcatore PRIMA di elementAt (elemento su cui è posizionato il mouse)
+        if (elementAt.classList.contains('box')) {
+            // .box
+            // console.log('drop prima di ', elementAt);
+            // elementAt.style.left = '10px';
+            // fino a quando elementAt ha un elemento successivo, sposto tutti di 10px left
+            while (test.nextElementSibling) {
+                test.nextElementSibling.classList.add('diff');
+                test = test.nextElementSibling;
+            }
+
+            // se il mouse si trova sullo stesso elemento (elementAt === dragSrcEl) non visualizzo lo span
+            if (dragSrcEl !== elementAt) elementAt.before(span);
+            // NOTE: con il before dell'elemento draggato c'è un problema:
+            // Quando il mouse è posizionato sul .box, questo si sposta e il elementAt non è più il .box ma il .container
+            // elementAt.before(dragSrcEl);
         } else {
-            // elementAt.style.left = `${dragSrcEl.offsetWidth}px`;
-            // elementAt.style.left = '100%';
-            // dragSrcEl.style.left = '-514px';
-            elementAt.before(dragSrcEl);
+            // .container
+            elementAt.appendChild(span);
+            // this.appendChild(dragSrcEl);
         }
+        // TEST: logica funzionante con un marcatore PRIMA di elementAt (elemento su cui è posizionato il mouse)
+        /* if (elementAt === dragSrcEl) return;
+        if (elementAt.classList.contains('container')) {
+            this.appendChild(dragSrcEl);
+        } else {
+            // console.clear();
+            // console.log('dragSrc : ', dragSrcEl.offsetLeft);
+            // console.log('elementAt before : ', elementAt.offsetLeft);
+            // console.log('diff : ', dragSrcEl.offsetLeft - elementAt.offsetLeft);
+            // console.log('width / 2: ', dragSrcEl.offsetWidth / 2);
+            if (dragSrcEl.offsetLeft - elementAt.offsetLeft <= (dragSrcEl.offsetWidth / 2)) {
+                // console.log(elementAt);
+                // elementAt.style.left = '-100%';
+                // const val = dragSrcEl.offsetLeft - elementAt.offsetLeft;
+                // elementAt.style.left = `-${dragSrcEl.offsetWidth}px`;
+                // dragSrcEl.style.left = '514px';
+                elementAt.after(dragSrcEl);
+            } else {
+                // elementAt.style.left = `${dragSrcEl.offsetWidth}px`;
+                // elementAt.style.left = '100%';
+                // dragSrcEl.style.left = '-514px';
+                elementAt.before(dragSrcEl);
+            }
+        } */
+        // span.remove();
 
         e.dataTransfer.dropEffect = 'move';
 
@@ -42,26 +77,25 @@ document.addEventListener('DOMContentLoaded', (event) => {
     }
 
     function handleDragEnter(e) {
+        console.log('drageneter');
         this.classList.add('over');
     }
 
     function handleDragLeave(e) {
+        console.log('dragLeave');
         this.classList.remove('over');
+        this.parentElement.querySelectorAll('.diff').forEach(el => el.classList.remove('diff'));
     }
 
-    function handleDrop(e) {
+    function handleContainerDrop(e) {
         if (e.stopPropagation) {
             e.stopPropagation(); // stops the browser from redirecting.
         }
-
-        // elementAt.before(dragSrcEl);
-        // console.log(dragSrcEl, this);
-        /* if (dragSrcEl != this) {
-            dragSrcEl.innerHTML = this.innerHTML;
-            this.innerHTML = e.dataTransfer.getData('text/html');
-        } */
-
-        return false;
+        dragSrcEl.classList.replace('el', 'box');
+        elementAt.classList.remove('over');
+        span.after(dragSrcEl);
+        span?.remove();
+        this.querySelectorAll('.diff').forEach(el => el.classList.remove('diff'));
     }
 
     function handleDragEnd(e) {
@@ -77,9 +111,17 @@ document.addEventListener('DOMContentLoaded', (event) => {
     items.forEach(function(item) {
         item.addEventListener('dragstart', handleDragStart, false);
         item.addEventListener('dragenter', handleDragEnter, false);
-        item.addEventListener('dragover', handleDragOver, false);
         item.addEventListener('dragleave', handleDragLeave, false);
-        item.addEventListener('drop', handleDrop, false);
         item.addEventListener('dragend', handleDragEnd, false);
     });
+    let elements = document.querySelectorAll('.el');
+    elements.forEach(function(item) {
+        item.addEventListener('dragstart', handleDragStart, false);
+        item.addEventListener('dragenter', handleDragEnter, false);
+        item.addEventListener('dragleave', handleDragLeave, false);
+        item.addEventListener('dragend', handleDragEnd, false);
+    });
+    document.querySelector('.container').addEventListener('drop', handleContainerDrop);
+    document.querySelector('.container').addEventListener('dragover', handleDragOver);
+
 });
