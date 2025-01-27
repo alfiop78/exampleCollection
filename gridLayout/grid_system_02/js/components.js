53687000da50a8f3a4ba21ef6e64ac79f5b74648
alfio pietrantuono <alfio@localhost.localdomain>
2025-01-27 16:53:48 +0100

    ottimizzazione main-nav e i relativi elementi .select .items

--- gridLayout/grid_system_02/js/components.js
+++ gridLayout/grid_system_02/js/components.js
@@ -15,11 +15,29 @@ document.querySelectorAll('button.btn-dropdown').forEach(dropdown => {
   })
 });
 
+// eventi sugli elementi all'interno di una dropdown
+document.querySelectorAll('.dropdown:not([multiple]) .items').forEach(li => {
+  li.addEventListener('click', handleSelectElement);
+});
+
+document.querySelectorAll('.dropdown[multiple] .items').forEach(li => {
+  li.addEventListener('click', handleMultiSelectElement);
+});
+
 const menu = document.getElementById('menu');
 menu.addEventListener('click', toggleNav);
 
 function toggleNav(e) {
   const main_nav = document.getElementById(e.currentTarget.dataset.referenceId);
   main_nav.classList.toggle('show');
+}
 
+function handleSelectElement(e) {
+  // visualizzo l'items selezionato nello span del <button> relativo
+  const button = document.querySelector(`button[data-dropdown-id='${e.currentTarget.parentElement.dataset.parent}']`);
+  button.dataset.value = e.currentTarget.innerText;
+}
+
+function handleMultiSelectElement(e) {
+  e.currentTarget.toggleAttribute("selected");
 }
